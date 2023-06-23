04395ec1cb74c41b9b2e5a19502444d3c4ace705
eniotnaCDMX <84527440+eniotnaCDMX@users.noreply.github.com>
2023-06-23 16:34:26 -0600

    Add files via upload

---
+++ NewProject.sh
@@ -0,0 +1,6 @@
+cd ~/Desktop
+mkdir -p ~/Desktop/NewPythonFolder
+git clone https://github.com/eniotnaCDMX/Requirements.git ~/Desktop/NewPythonFolder
+cd ~/Desktop/NewPythonFolder
+chmod 777 Venv_config.sh
+./Venv_config.sh
